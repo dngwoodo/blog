766bdbae7aef48c4fe5452efdd13064e07d964fa
dngwoodo <dngwoodo@gmail.com>
2022-01-05 18:04:32 +0900

    fix: header 컴포넌트내용 및 스타일 수정

--- src/components/page-header/index.js
+++ src/components/page-header/index.js
@@ -28,16 +28,19 @@ function PageHeader({ siteTitle }) {
         <header className="page-header-wrapper">
           <div className="page-header">
             <div className="front-section">
-              <Link className="link" to="/">
+              <Link className="link link-title" to="/">
                 {siteTitle}
               </Link>
             </div>
             <div className="trailing-section">
-              <Link className="link" to="/about">
-                about
+              <Link className="link" to="https://inflearn.com">
+                인프런
               </Link>
               <Link className="link" to="/posts">
-                posts
+                포스트
+              </Link>
+              <Link className="link" to="https://www.inflearn.com/pages?type=withus">
+                채용
               </Link>
               <PostSearch
                 posts={data.allMarkdownRemark.edges.map(({ node }) => new Post(node, true))}
