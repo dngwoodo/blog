d369599e7f983bec11fafe9dc4c2562464e9b655
dngwoodo <dngwoodo@gmail.com>
2022-01-05 18:57:59 +0900

    up: post-header 컴포넌트 구조 변경

--- src/components/post-header/index.js
+++ src/components/post-header/index.js
@@ -6,23 +6,24 @@ function PostHeader({ post, viewCount }) {
   return (
     <header className="post-header">
       {post.emoji && <div className="emoji">{post.emoji}</div>}
+      <h1 className="title">{post.title}</h1>
       <div className="info">
+        <div className="info-top">
+          <span className="author">
+            posted by <strong>{post.author}</strong>,
+          </span>{' '}
+          <span>
+            {post.date}
+            {viewCount && ` · ${viewCount} views`}
+          </span>
+        </div>
         <div className="categories">
           {post.categories.map((category) => (
             <Link className="category" key={category} to={`/posts/${category}`}>
-              {category}
+              #{category}
             </Link>
           ))}
         </div>
-      </div>
-
-      <h1 className="title">{post.title}</h1>
-      <div className="info">
-        <div className="author">
-          posted by <strong>{post.author}</strong>,
-        </div>{' '}
-        {post.date}
-        {viewCount && ` · ${viewCount} views`}
       </div>
     </header>
   );
